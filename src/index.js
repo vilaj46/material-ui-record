906d0e8ab8f74e0fc7425e609f653d940b2411e8
Julian <vilaj46@gmail.com>
2020-12-07 17:27:46 -0500

    Testing commit.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from "./components/App/App";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 /**
+ * Testing
+ *
  * More functionality on the Positioning page (eventually).
- * 
+ *
  * Titles needs more functionality; delete & insert.
- * 
+ *
  * Add an 'x' at the top right of the modals, closing it instead of clicking off.
  */
