47c797612566c6b15a51983d3e633658cb811ae1
Julian <vilaj46@gmail.com>
2021-01-27 09:53:47 -0500

    Cleaned PageNumberFields and separated error handling in outsider function.

--- src/components/Modal/modals/HeaderModal/subs/PageNumbersTab/subs/PageNumberFields/PageNumberFields.js
+++ src/components/Modal/modals/HeaderModal/subs/PageNumbersTab/subs/PageNumberFields/PageNumberFields.js
@@ -28,47 +28,8 @@ function PageNumberFields({ headers }) {
   const [endError, setEndError] = useState(false);
   const [startError, setStartError] = useState(false);
 
-  // const onChange = (e, k) => {
   function onChange(e, k) {
     const { value } = e.target;
-
-    // Is value a number.
-    // const isNumber = isValidNumber(value);
-
-    // let sError = startError; // Page number start error.
-    // let eError = endError; // Page number end error.
-    // let pageNumberStart = Number(pageRange.start);
-    // let pageNumberEnd = Number(pageRange.end);
-
-    // // Set up values based on what we have changed.
-    // if (k === "start" && isNumber === true) {
-    //   pageNumberStart = Number(value);
-    // } else if (k === "end" && isNumber === true) {
-    //   pageNumberEnd = Number(value);
-    // }
-
-    // // If our starting value is < end value, we have an error.
-    // if (pageNumberStart > documentPages) {
-    //   sError = true;
-    // } else if (pageNumberEnd > documentPages) {
-    //   eError = true;
-    // } else if (
-    //   pageNumberEnd < pageNumberStart &&
-    //   pageNumberStart <= documentPages
-    // ) {
-    //   eError = true;
-    //   sError = false;
-    // } else {
-    //   sError = false;
-    //   eError = false;
-    // }
-
-    // // If the value is not a number, error is true.
-    // if (isNumber === false && k === "start") {
-    //   sError = true;
-    // } else if (isNumber === false && k === "end") {
-    //   eError = true;
-    // }
 
     const currentErrors = { startError, endError };
     const { sError, eError } = getErrors(
--- src/components/Modal/modals/HeaderModal/subs/PageNumbersTab/subs/PageNumberFields/helpers/getErrors.js
+++ src/components/Modal/modals/HeaderModal/subs/PageNumbersTab/subs/PageNumberFields/helpers/getErrors.js
@@ -17,31 +17,90 @@ function getErrors(k, value, currentErrors, pageRange, documentPages) {
     pageNumberStart = Number(value);
   } else if (k === "end" && isNumber === true) {
     pageNumberEnd = Number(value);
-  }
-
-  if (pageNumberStart > documentPages) {
-    sError = true;
-  } else if (pageNumberEnd > documentPages) {
-    eError = true;
-  } else if (
-    pageNumberEnd < pageNumberStart &&
-    pageNumberStart <= documentPages
-  ) {
-    eError = true;
-    sError = false;
   } else {
-    sError = false;
-    eError = false;
+    // The value we changed is not a number.
+    const notANumberErrors = valueIsNotANumber(k, sError, eError);
+    return notANumberErrors;
   }
 
-  // If the value is not a number, error is true.
-  if (isNumber === false && k === "start") {
-    sError = true;
-  } else if (isNumber === false && k === "end") {
-    eError = true;
-  }
+  const errors = {
+    sError,
+    eError,
+  };
+  const currentPageNumbers = {
+    pageNumberStart,
+    pageNumberEnd,
+  };
+
+  // Starting value should be less than or equal to ending value.
+  // Ending value should be greater than or equal to starting value.
+
+  // Starting value should be less than or equal to document page count.
+  // Ending value should be less than or equal to document page count.
+  const documentPagesErrors = checkAgainstDocumentPages(
+    k,
+    errors,
+    currentPageNumbers,
+    documentPages
+  );
+
+  sError = documentPagesErrors.sError;
+  eError = documentPagesErrors.eError;
 
   return { sError, eError };
+}
+
+/**
+ * @param {String} k - 'start' or 'end' from the Pages From input.
+ * @param {Object} currentErrors - Object with sError and eError.
+ * @param {Object} currentPageNumbers - Object with our current page numbers with the new number also.
+ * @param {Number} documentPages - Page count of our PDF document.
+ * @return {Object} New errors after checking.
+ *
+ * Check which value we are changing. Then check whether or not the page count is
+ * over the document. If it is, set value to true because of the error.
+ * If it isn't set document pages error to false.
+ */
+function checkAgainstDocumentPages(
+  k,
+  currentErrors,
+  currentPageNumbers,
+  documentPages
+) {
+  const { pageNumberStart, pageNumberEnd } = currentPageNumbers;
+
+  if (k === "start" && pageNumberStart > documentPages) {
+    currentErrors.sError = true;
+  } else if (k === "start") {
+    currentErrors.sError = false;
+  } else if (k === "end" && pageNumberEnd > documentPages) {
+    currentErrors.eError = true;
+  } else if (k === "end") {
+    currentErrors.eError = false;
+  }
+
+  return currentErrors;
+}
+
+/**
+ * @param {String} k - Key: Either 'start' or 'end' in the Pages From inputs.
+ * @param {Boolean} sError - Our current starting error.
+ * @param {Boolean} eError - Our current ending error.
+ * @return Object with the starting error and ending error.
+ *
+ * Since the value we changed is not a number, check to see which key we
+ * changed and make the error to true. Then return the new error
+ * and the current error of the other input.
+ */
+function valueIsNotANumber(k, sError, eError) {
+  if (k === "start") {
+    sError = true;
+    return { sError, eError };
+  } else {
+    // k is 'end'
+    eError = true;
+    return { sError, eError };
+  }
 }
 
 export default getErrors;
